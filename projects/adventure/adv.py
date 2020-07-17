e8eaaa26250960deaf882ff6ebf55971e7198c2a
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-07-17 07:52:51 -0700

    cross test passes with 1 extra move still

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -24,10 +24,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -42,7 +42,7 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 def print_room_and_exits():
     pass
-    print(f"* Room {player.current_room.id}, Exits: {player.current_room.get_exits()} *")
+    print(f" - ROOM - : {player.current_room.id}, - EXITS - : {player.current_room.get_exits()}")
 
 # takes in a direction string, and returns the opposite direction as a string
 def get_opposite_direction(direction):
@@ -66,6 +66,56 @@ def traverse(rand_dir):
     player.travel(rand_dir, True)
     print_room_and_exits()
 
+# from Player class
+# def travel(self, direction, show_rooms = False):
+#         next_room = self.current_room.get_room_in_direction(direction)
+#         if next_room is not None:
+#             self.current_room = next_room
+#             if (show_rooms):
+#                 next_room.print_room_description(self)
+#         else:
+#             print("You cannot move in that direction.")
+
+# # From Graph, replace starting_vertex with current room
+# def dft(self, starting_vertex):
+#         """
+#         Print each vertex in depth-first order
+#         beginning from starting_vertex.
+#         """
+#         print("DFT called (pt 3)")
+
+#         # make a stack
+#         stack = Stack()
+
+#         # push on our starting node
+#         stack.push(starting_vertex)
+
+#         # make a Set to track if we've been here before
+#         visited = set()
+
+#         # while our stack isn't empty
+#         while stack.size() > 0:
+
+#             # pop off whatever's on top, this is current_node
+#             current_node = stack.pop()
+
+#             # if we haven't visited this vertex before
+#             if current_node not in visited:
+
+#                 # mark as visited
+#                 visited.add(current_node)
+
+#                 # print it
+#                 print(f"current_node = {current_node}")
+
+#                 # get its neighbors
+#                 neighbors = self.get_neighbors(current_node)
+
+#                 #for each of the neighbors  
+#                 for neighbor in neighbors:
+#                     # add to the Stack
+#                     stack.push(neighbor)
+
 # do main work here
 def search_all_rooms():
 
@@ -75,68 +125,116 @@ def search_all_rooms():
     # keeps while loop going
     stack = Stack()
 
-    # Rooms visited. Key = id, Value = [exits] ?
+    # Rooms visited. Key = id, Value = [exits: id] ?
     visited = {}
     visited_count = 0
 
-    # keep track of the room we came from and direction
-    last_room = None
-    last_direction = ""
-
-    # keep track of how to get back to last fork
-    back_track_path = []
-
-    #
+    # key is room id, value is exits that aren't explored
     paths_to_explore = {}
 
     # 1. Start at Room 0
     print_room_and_exits()
+
+    test = player.current_room.get_room_in_direction("n")
+    print(f"test = {test.id}")
+
+    # paths_to_explore[player.current_room.id] = player.current_room.get_exits()
+
+    # 1.2. Head north to start off
+    # traverse("n")
+
+    # add to visited
+    # visited[player.current_room.id] = dict()
+    # path.append("n")
+    # neighbors = player.current_room.get_exits()
+    # paths_to_explore[player.current_room.id] = []
+    # for room in neighbors:
+    #     print(f"room = {room}")
+    #     # not done with this?
+    #     visited[player.current_room.id][room] = "?"
+    #     paths_to_explore[player.current_room.id].append(room)
+    # set last room
+    last_room = 0
+
+    # set last direction
+    last_direction = "end"
+
+    # back track (for when you hit a dead end)
+    back_track_path = Stack()
+
+    
     stack.push(player.current_room.id)
+    # neighbors = player.current_room.get_exits()
+        # paths_to_explore[player.current_room.id] = [] # ?
+    #     last_direction = "end" 
+    #     for room in neighbors:
+    #         print(f"room = {room}")
+    #         # check which rooms have been explored already
+    #         # rand_dir = pick_random_direction(neighbors)
+            
+    #         if player.current_room.get_room_in_direction(room).id not in visited:
+    #             last_direction = room
 
-    while stack.size() > 0:
+    # print(f"paths to explore = {paths_to_explore}")
+    exits = []
 
-        if visited_count == len(room_graph):
+    total_rooms = len(room_graph)
+    while True:
+
+        if visited_count == total_rooms:
             print("all rooms visited, breaking")
             break
 
-        current = stack.pop()
-        last_room = current
+        # # add to visited and increment count
+        # if player.current_room.id not in visited:
+        #     visited[player.current_room.id] = dict()
+        #     visited_count += 1
+        # # else:
         
+        # possible exits for current room
+        # exits = paths_to_explore[player.current_room.id]
+        if last_direction == "end":
+            # dead end
+            print("Dead End")
+            print(f"last_direction = {last_direction}")
+            last_direction = back_track_path.pop()
+            path.append(last_direction)
+            traverse(last_direction)
+        else:
+            # pick random direction
+            # rand_dir = pick_random_direction(exits)
+            # print(f"random direction: {rand_dir}")
+            
+            path.append(last_direction)
+            back_track_path.push(get_opposite_direction(last_direction))
+            traverse(last_direction)
 
-        # print(f"current = {current}")
-        # if we are in a room we have NOT visited yet, add it to visited
-        if current not in visited:
             # add to visited
-            visited[current] = dict()
+            visited[player.current_room.id] = dict()
+            exits = player.current_room.get_exits()
+            for exit in exits:
+                visited[player.current_room.id][exit] = player.current_room.get_room_in_direction(exit).id
             visited_count += 1
-            # add key (id) and value (dict of exits, where key is direction and value is id)
-            # then, get this room's exits and add them to the value of visited[current] value dict
-            neighbors = player.current_room.get_exits()
-            paths_to_explore[current] = []
-            for room in neighbors:
-                # print(f"room = {room}")
-                # not done with this?
-                visited[current][room] = "?"
-                paths_to_explore[current].append(room)
 
-        # print(f"paths_to_explore for {current} = {paths_to_explore[current]}")
-        # print(f"visited: {visited}")
+            print(f"visited[{player.current_room.id}]: {visited[player.current_room.id]}")
+            print(f"{player.current_room.id}: {visited[player.current_room.id]}")
+            print(f"last direction = {last_direction}")
 
-        # 2. Check exits [n, s, w, e] (pick randomint from 1-4, n=1,s=2,etc...)
-        rand_dir = pick_random_direction(player.current_room.get_exits())
-        # print(f"random direction: {rand_dir}")
 
-        # add direction we will go in to the path
-        path.append(rand_dir)
-        # print(f"path now: {path}")
-
-        # 4. Take random exit that has NOT been visited ?
-        last_direction = rand_dir
-        traverse(rand_dir)
-        stack.push(player.current_room.id)
-        # now we can add the exits value opposite key to the last room id we were in
-    
-    # print(f"last room = {last_room}")
+        neighbors = player.current_room.get_exits()
+        # paths_to_explore[player.current_room.id] = []
+        last_direction = "end"
+        # for each neighbor
+        for room in neighbors:
+            print(f"room = {room}")
+            # check which rooms have been explored already
+            # rand_dir = pick_random_direction(neighbors)
+            
+            if player.current_room.get_room_in_direction(room).id not in visited:
+                last_direction = room
+        
+    print(f"visited = {visited}")
+    print(f"paths to explore = {paths_to_explore}")
             
     return path
 
